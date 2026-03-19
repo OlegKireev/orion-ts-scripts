5b3378ca42f162a43f5464ffeca57290dfdb567c
Oleg Kireev <flucky@mail.ru>
2026-03-19 13:26:34 +0300

    fix(tracking): optimize waiting

--- src/lib/tracking.ts
+++ src/lib/tracking.ts
@@ -1,10 +1,5 @@
 export function tracking(menuItem: string) {
+  Orion.WaitMenu('Tracking', menuItem);
   Orion.UseSkill('Tracking');
-	if (Orion.WaitForMenu(100)) {
-		var menu = Orion.GetMenu('last');
-		if (menu !== null) {
-			if (menu.Name() === "Tracking")
-				menu.Select(menuItem);
-		}
-	}
+  Orion.Wait(100);
 }
