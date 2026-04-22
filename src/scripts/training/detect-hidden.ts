4393dfdae8f15263479d7e0c5e2388c877f1e76d
Oleg Kireev <flucky@mail.ru>
2026-04-22 13:40:09 +0300

    fix(detect-hidden): disable warmode

--- src/scripts/training/detect-hidden.ts
+++ src/scripts/training/detect-hidden.ts
@@ -3,6 +3,7 @@ export { Eating } from '@/lib/eating';
 export function DetectHidden() {
   Orion.Exec('Eating');
   while (true) {
+    Orion.WarMode(0);
     Orion.UseSkill('Detecting Hidden');
     Orion.Wait(2500);
   }
@@ -11,6 +12,7 @@ export function DetectHidden() {
 export function DetectHiddenBot() {
   while (true) {
     if (!Player.Hidden()) {
+      Orion.WarMode(0);
       Orion.UseSkill('Hiding');
       Orion.Wait(6000);
     }
